fdee79eadf7d6d10f5e874f6f09bc6a35c9113a8
Gowthami742 <Gowthamirajesh07@gmail.com>
2026-06-09 19:49:49 +0530

    Typeconversion.py

--- Typeconversion.py
+++ Typeconversion.py
@@ -22,3 +22,30 @@ print(int(x))'''
 '''x=int(5.8)
 y=int(3)
 print(x+y)'''
+
+#Type Casting
+#Implicit Type casting
+
+'''x=5 #int
+y=2.5 #float
+result=x+y
+print(result)
+print(type(result))'''
+
+#Explicit Type casting
+'''x="10"
+y=int(x)
+print(y)
+print(type(y))'''
+
+#Output and input in Python
+#output
+print("Gowthami")
+
+#input
+'''age = input("Enter age: ")
+print(age)
+print(type(age)) '''
+
+
+
