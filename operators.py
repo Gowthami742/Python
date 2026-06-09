5c2ceb5dbbb8c3adbf48182448b46856c55be594
Gowthami742 <Gowthamirajesh07@gmail.com>
2026-06-09 21:27:07 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,26 @@
+#Operators
+#Arithmetic operators
+
+a=10
+b=3
+print(a+b) #addition
+print(a-b) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a//b) #floor division
+print(a%b) #Remainder
+print(a**b) #Modulus
+
+#Assignment Operators
+
+x=2
+x+=2 # x=x+2
+print(x)
+x-=2 # x=x-2 (here x is 4 now)
+print(x)
+x*=2 #x=x*2 (here x is 2 now)
+print(x)
+x/=2 #x=x/2(here x is 4 now )
+print(x)
+x%=2 #x=x%2(here x is 2 now )
+print(x)
