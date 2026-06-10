5b82021578d76fee17e401442cc801edc3bb1d89
Gowthami742 <Gowthamirajesh07@gmail.com>
2026-06-10 18:06:06 +0530

    operators.py

--- operators.py
+++ operators.py
@@ -1,6 +1,6 @@
 #Operators
 #Arithmetic operators
-
+'''
 a=10
 b=3
 print(a+b) #addition
@@ -8,19 +8,86 @@ print(a-b) #subtraction
 print(a*b) #multiplication
 print(a/b) #division
 print(a//b) #floor division
-print(a%b) #Modulus
-print(a**b) #Exponentiation
+print(a%b) #Remainder
+print(a**b) #Modulus '''
+
+'''price =200
+gst = 18
+total= price + (price*gst/100)
+print(total)'''
 
 #Assignment Operators
 
-x=2
-x+=2 # x=x+2
-print(x)
-x-=2 # x=x-2 (here x is 4 now)
-print(x)
-x*=2 #x=x*2 (here x is 2 now)
-print(x)
-x/=2 #x=x/2(here x is 4 now )
-print(x)
-x%=2 #x=x%2(here x is 2 now )
-print(x)
+balance = 5000
+
+'''x=5
+x+=3 # x=x+3 i.e 5+3=8
+print(x)'''
+
+'''carttotal=1000
+carttotal +=500 #added an new item
+print(carttotal)'''
+
+'''x=10
+x-=4 # x=x-4 i.e 10-4=6
+print(x) '''
+
+'''carttotal =5000
+carttotal-=1500 #subtract  new item
+print(carttotal) '''
+
+'''x=2
+x*=5 # x=x*2 i.e 5*2=10
+print(x) '''
+ 
+'''carttotal=5000
+carttotal*=1500 #Multiply  new item
+print(carttotal)'''
+
+"""x=10
+x%=3 # x=x/3 i.e 10/3=1
+print(x) """
+
+"""x=10
+x//=3 # x=x//3 i.e 10/3=3
+print(x) """
+
+'''wallet=1000
+wallet+=500
+wallet-=200
+wallet*=2
+print("Wallet balance:",wallet) '''
+
+'''=:Store
++=:add
+-=:sub
+*=:Multiply
+/=:Divide
+%=:Remainder/Modulus
+//=:Floor division
+**=: power & store'''
+
+
+#Comparsion/ Relational Operators
+
+'''savedpassword = "Python1234"
+enteredpassword = input("Enter password:")
+print(savedpassword == enteredpassword)'''
+
+'''age = 20
+print(age>21)'''
+
+'''stock = 5
+print(stock<4) '''
+
+'''marks = 35
+print(marks >=35)'''
+
+'''marks = int(input("Enter marks:"))
+print("pass:",marks>=35)
+print("distinction:",marks>=75)'''
+
+#Logical Operators 
+
+
+
