7e526e85487975ca54feb861cf5352b02a435d14
Gowthami742 <Gowthamirajesh07@gmail.com>
2026-06-23 18:59:45 +0530

    Add files via upload

---
+++ conditionalstmnts.py
@@ -0,0 +1,55 @@
+#conditional statements
+#if statement
+
+'''marks = 40
+if marks >= 35:
+    print("pass")'''
+
+
+    #if-else statement
+'''marks = 40
+if marks >= 45:
+        print("pass")
+else:
+        print("fail")'''
+
+        #if -elif-else statement
+
+'''marks = 82
+if marks >= 90:
+        print("pass")
+elif marks >= 70:
+        print("A grade")
+elif marks >=60:
+        print("B Grade")
+else:
+        print("fail")'''
+
+
+'''age =18
+if age >=18:
+    print("Eligible for vote")
+else:
+    print("Not eligible")'''
+
+
+'''age =16
+if age >=18:
+    print("Eligible for vote")
+elif age<=17:
+    print("Not eligible")
+elif age<=16:
+    print("Not eligible")
+else:
+    print("Not elogible")'''
+
+
+#Nested if condition
+
+
+
+     
+
+
+
+          
