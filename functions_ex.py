2ef39c9c6eac90a79e03bb14c107f9dfa02062d6
Gowthami742 <Gowthamirajesh07@gmail.com>
2026-06-30 20:05:17 +0530

    Add files via upload

---
+++ functions_ex.py
@@ -0,0 +1,118 @@
+#Functions 
+
+# def cal_sum(a, b):
+#     sum = a + b
+#     print(sum)
+#     return sum 
+
+# cal_sum(2,5)
+
+# cal_sum(7,8)
+
+# cal_sum(10,20)
+
+# funsction definition 
+# def cal_sum(a, b): # paramaeters
+#     return a + b
+
+# sum = cal_sum(10,35) # function call ; 10,35 are -- arguments
+# print(sum)
+
+
+# average of three numbers 
+
+# def calc_avg (a, b, c):
+#     sum = a + b + c
+#     avg = sum / 3
+#     print(avg)
+#     return avg
+
+# calc_avg ( 3 , 4, 2)
+    
+
+# print("apnacollge", end="$") #sep = " "
+# print("shradhakhapra") #end = "\n"
+    
+# def cal_sum(a, b):
+#     sum = a + b
+#     return sum 
+
+# sum = cal_sum (20,30)
+# print(sum)
+
+
+# def calc_avg(a, b, c):
+#     sum = a + b + c
+#     avg = sum/3
+#     print(avg)
+#     return avg
+
+# calc_avg(3, 4, 5)
+
+
+# default parameters
+
+# def calc_prod(a = 2, b = 4): # where we can assign the values to parameters while defining function
+#     print(a * b)
+#     return a * b
+
+# calc_prod()
+
+
+#WAF to print the length of a list. ( list is the parameter)
+
+# cities = ["delhi","gurgaon","noida","pune","mumbai","chennai"]
+# heroes = ["thor","iron man","captaion america","shakti man"]
+
+# def print_len(list):
+#     print(len(list))
+
+#     print_len(cities)
+#     print_len(heroes)
+
+#WAF to find the factorial of n. (n is the parameter)
+# n = 5
+# def cal_fact(n):
+#     fact = 1
+# for i in range (1,n+1):
+#      fact *= i
+#      print(fact)
+
+#      cal_fact(5)
+
+     #WAF to convert USD to INR.
+
+# def converter(usd_val):
+#           inr_val = usd_val * 83
+#           print(usd_val, "USD = ", inr_val, "INR" )
+
+#           converter(73)
+
+
+# def cal_sum(a, b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# cal_sum (2,4)
+
+
+# def cal_prod (a , b):
+#     prod = a * b
+#     print(prod)
+#     return prod
+
+
+# cal_prod(5, 10) 
+
+
+def cal_avg(a, b , c):
+    sum = a + b + c
+    avg = sum/3
+    print(avg)
+    return avg
+
+cal_avg(3, 4 ,5)
+
+
+
